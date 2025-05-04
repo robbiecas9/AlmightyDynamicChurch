6ce4622262445c3f005db2c4aea9c3e6b5cf0979
Robin B <robbiecas92@gmail.com>
2025-05-04 11:43:09 +0400

    Fix: Bind server to process.env.PORT for Render

--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = parseInt(process.env.PORT || "5000", 10); // fallback to 5000 locally
   server.listen({
     port,
     host: "0.0.0.0",
@@ -67,4 +67,4 @@ app.use((req, res, next) => {
   }, () => {
     log(`serving on port ${port}`);
   });
-})();
+  
